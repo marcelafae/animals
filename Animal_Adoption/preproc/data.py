71360cd80d20b06fbabed2d3d4fbc37a5611e0b7
John Galindo <johnalexgalindop@gmail.com>
2023-03-09 12:53:38 +0100

    Made changes

--- Animal_Adoption/preproc/data.py
+++ Animal_Adoption/preproc/data.py
@@ -8,8 +8,8 @@ def get_data():
     Its values should be pandas.DataFrames loaded from csv files
     """
     # load the dataset
-    #root_dir = os.path.dirname(os.path.dirname(__file__))
-    csv_path = os.path.join("../../raw_data", "aac_intakes_outcomes.csv")
+    root_dir = os.path.dirname(os.path.dirname(__file__))
+    csv_path = os.path.join(root_dir, "../raw_data", "aac_intakes_outcomes.csv")
     data= pd.read_csv(os.path.join(csv_path))
 
     # Drop duplicates in place
@@ -31,7 +31,7 @@ def get_data():
     data.drop_duplicates(subset=["animal_id_outcome"], keep="first", inplace=True)
     data[data["animal_id_outcome"].duplicated()]["animal_id_outcome"].unique()
 
-    # relabled colums: split the column 'sex_upon_outcome' into a column 'sex' and a column 'sex_type' 
+    # relabled colums: split the column 'sex_upon_outcome' into a column 'sex' and a column 'sex_type'
     data['sex_type']= data.sex_upon_outcome.map(lambda x : x.split(" ")[0])
     data['sex']= data.sex_upon_outcome.map(lambda x : x.split(" ")[-1])
     # relabel the values of the new column 'sex' to value 'Neutered' and value 'Intact' and value 'NaN'(unknown)
@@ -47,16 +47,16 @@ def get_data():
     return data
     # if data:
     #     return data
-    # else: 
+    # else:
     #     return "Data can't be loaded."
-        
-# relable the column 'breed'     
+
+# relable the column 'breed'
 def get_breed (series: pd.Series) -> pd.Series:
     def cleaning(d: str):
         ## remove whitespaces
-        d = d.strip() 
-        ## lowercasing 
-        d = d.lower() 
+        d = d.strip()
+        ## lowercasing
+        d = d.lower()
         ## remove the word 'mix'-> doesn't work -> data['breed']= data.breed.str.replace('mix', '')
         d = d.replace('mix', '')
         return d
@@ -69,12 +69,12 @@ def get_breed (series: pd.Series) -> pd.Series:
         "long_hair": ["domestic longhair", "long hair", "longhair"],
         "uncommon": get_uncommon_breeds(series),
     }
-    
+
     for key, val in breed_map.items():
         series = replace_breeds(val, key, series)
         # series = series.map(uncommon???)
     return series
-    
+
     # def unknown(d):
 
 def replace_breeds(breed_list: list,
