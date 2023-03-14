8a42fdb551934029ae17a4b3aab5d081c45c6b24
marcela fae <marcela@1982.us>
2023-03-14 12:52:56 +0100

    function for common/uncommon breeds (data_dog.py), improvements on the front end (app.py) and background imgs for the dog/cat

--- Animal_Adoption/app/app.py
+++ Animal_Adoption/app/app.py
@@ -6,67 +6,146 @@ import os
 
 curr_path = os.path.dirname(__file__)
 
-with open(os.path.join(curr_path, 'style.css'), encoding='utf-8') as f:
-    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+st.set_page_config(
+    page_title = 'Animals deserve a home',
+    page_icon = '🐶🐱',
+    layout = 'wide', #centered
+    initial_sidebar_state = 'auto', # collapsed
+
+)
+
+
+CSS_dog = """
+.block-container {
+    background-color: rgba(255, 255, 255, 0.5);
+    color: rgba(0, 0, 0, 1);
+}
+.stApp {
+    background-image: url(https://tractive.com/blog/wp-content/uploads/2016/04/puppy-care-guide-for-new-parents.jpg);
+    background-size: cover;
+}
+"""
+
+CSS_cat = """
+.block-container {
+    background-color: rgba(255, 255, 255, 0.5);
+    color: rgba(0, 0, 0, 1);
+}
+.stApp {
+    background-image: url('https://wallpapercave.com/wp/CKJtGTz.jpg');
+    background-size: cover;
+}
+"""
+
+#Title
+'''# 🐕🐩 Animals deserve a home 🐈‍⬛🐈'''
+
+#Rest
+st.markdown('#### A project to help animal shelters worldwide')
+
+st.write(' ###### By providing some information, our website can assist you in determining how long a dog or cat will stay in your shelter')
+st.write('First, you need to specify if the animal is a dog or a cat.')
+choose = st.radio('',('','🐩 Dog','🐈 Cat'))
+
+if choose == '':
+    pass
+elif choose == '🐩 Dog':
+    st.write(f'<style>{CSS_dog}</style>', unsafe_allow_html=True)
+    animal_type = 'Dog'
+elif choose == '🐈 Cat':
+    st.write(f'<style>{CSS_cat}</style>', unsafe_allow_html=True)
+    animal_type = 'Cat'
+
+
+#THIS PART NEEDS TO BE 100% AN ACCURATE MATCH W THE MODEL!!!!!!!!!!!!!!!!!!!!!!!!
+
+# Conditions
+st.write("### Can you tell me more about how this animal was found?")
+
+col1, col2, col3 = st.columns((2,2,3))
+
+#check the column name and what I wrote here
+intake_type = col1.selectbox("Pick one of those options about how this animal ended up in your shelter?",\
+    ('Public Assist', 'Owner Surrender', 'Stray', 'Euthanasia Request','Abandoned'))
+
+intake_condition = col2.selectbox("Now plase tell us about this animal health conditions",\
+    ('Normal', 'Injured', 'Aged', 'Sick', 'Other', 'Medical', 'Feral', 'Pregnant', 'Nursing', 'Behavior'))
 
 
 
-# st.markdown(
-#     f"""
-#     <style>
-#         {open("style.css").read()}
-#     </style>
-#     """,
-#     unsafe_allow_html=True
-# )
+st.write("### The age of this animal in years and months")
+col1, col2, col3 = st.columns((1.5,1.5,5))
+
+years = col1.number_input('Years',min_value = 0, max_value = 28,step=1,value=0)
+
+if years == 0:
+    months = col2.number_input('Months',min_value = 1, max_value = 11,step=1,value=1)
+else:
+    months = col2.number_input('Months',min_value = 0, max_value = 11,step=1,value=1)
+
+age_upon_intake_months = years * 12 + months
 
 
-st.title('Animals deserve a home')
-st.markdown('## A project to help animal shelters worldwide')
 
-# This front queries from our API [Animals_API](http://127.0.0.1:8000/predict?vgg_cat_or_dog=%E2%80%98Cat%E2%80%99&vgg_color=%E2%80%98Black%E2%80%99&vgg_breed=%E2%80%99Sheperd%E2%80%99&sex=%E2%80%98Female%E2%80%99&condition=%E2%80%99Normal%E2%80%99&age_animal=2&castraded=%E2%80%98Yes%27')
-# '''
-
-st.header('Photo')
-st.markdown('Now upload the photo of the new animal')
-
-uploaded_photo = st.file_uploader('here is how ppl can upload a photo')
+st.write("### Animal Characteristics")
+col1, col2, col3, col4, col5= st.columns((2,1.5,1.5,3,5))
 
 
-st.header('Questions')
-st.markdown('**Before** pressing submit, make sure you fill all fields below')
 
-with st.form(key='params_for_api'):
-    vgg_cat_or_dog=st.selectbox('Is this a cat or a dog?', ['Dog', 'Cat'])
-    vgg_color=st.multiselect('Choose the color of this animal.', ['Black', 'White', 'Brown', 'Beige','Has Spots', 'Tabby or Brindle' 'Tricolor'])
-    st.markdown('**Make sure you only select 1 or 2 colors. If the animal has 3 colors or spots or stripes/pattern, pick just 1 category')
-    vgg_breed=st.selectbox('More information about this animal. Pick one', ['Big House Dog', 'Small House Dog', 'Long hair Cat', 'Short Hair Cat', 'Uncommon'])
-    sex=st.selectbox('Sex of the animal', ['Male', 'Female'])
-    condition=st.selectbox('Animal condition', ['Normal', 'Sick', 'Injured', 'Nursing', 'Pregnant', 'Senior', 'Feral'])
-    age_animal=st.number_input('What is the age of this animal? If below 0 years, say 0', min_value=0, max_value=25, step=1, value=1)
-    castraded=st.checkbox('This animal has been castrated')
+good_with_other_dogs = col1.selectbox("Friendliness with dogs",('1','2', '3', '4', '5'))
+
+color = col2.selectbox("Color or pattern",('Black', 'White', 'Brown', 'Beige','Has Spots', 'Tabby or Brindle', 'Tricolor'))
+
+gender = ("Female", "Male")
+options = list(range(len(gender)))
+male_or_female_intake = col3.selectbox("Sex", options, format_func=lambda x: gender[x])
+
+if male_or_female_intake == 0:
+    spayed = ("Intact", "Spayed")
+    options = list(range(len(spayed)))
+    neutered_or_spayed_intake = col4.selectbox("Intact, Spayed or Neutered?", options, format_func=lambda x: spayed[x])
+else:
+    neutered = ("Intact", "Neutered")
+    options = list(range(len(neutered)))
+    neutered_or_spayed_intake = col5.selectbox("Intact, Spayed or Neutered?", options, format_func=lambda x: neutered[x])
 
 
-    submitted = st.form_submit_button("Lets see how many days...")
+st.write(f"### Please press the button to check if the animal will stay longer than 7 days in the shelter.")
+
+# Prediction
+
+url = 'https://animal-api-gzsqtwobpa-lz.a.run.app/predict'
+
+if st.button('Click here'):
+    if choose == '':
+        st.warning('You forgot to choose an animal!')
+    else: #please make sure this is correct with the model!!!!!
+        params = {"intake_type": intake_type,
+            "animal_type": animal_type,
+            "intake_condition": intake_condition,
+            "breed": breed,
+            "age_upon_intake_months": age_upon_intake_months,
+            "neutered_or_spayed_intake": neutered_or_spayed_intake,
+            "male_or_female_intake": male_or_female_intake,
+            "color": color
+        }
+
+        response = requests.get(url, params=params)
+        if response.json()["prediction"] == 0:
+            st.success("This animal is likely to stay less than one week in your shelter!")
+        else:
+            st.info('''
+            This animal is likely to stay longer 😿 Here are some things you can try:
+            - Take photos and videos of this animal to show their personality;
+            - Contact local newspapers, radio stations, and TV channels to see if they are willing to do a feature on the animal;
+            - artner with rescue groups;
+            - Social media can always be helpful! Make sure to use hashtags to increase reach.
+            '''
+            )
 
 
-params = dict(
-    vgg_cat_or_dog=vgg_cat_or_dog,
-    vgg_color=vgg_color,
-    vgg_breed=vgg_breed,
-    sex=sex,
-    condition=condition,
-    age_animal=age_animal,
-    castraded=castraded)
 
-animals_api_url = 'https://animal-api-gzsqtwobpa-lz.a.run.app/predict'
-response = requests.get(animals_api_url, params=params)
-
-prediction = response.json()
-
-pred = prediction['Days_in_shelter']
-
-st.header(f'Days in Shelter: ${round(pred, 2)}')
 
  #---------
  # TaxiFareModel front
