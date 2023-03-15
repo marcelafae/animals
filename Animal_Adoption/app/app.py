ba7b63d607ee06710b2b762f87f93e83ed4b8153
marcela fae <marcela@1982.us>
2023-03-15 09:32:35 +0100

    commit from yesterday didnt work

--- Animal_Adoption/app/app.py
+++ Animal_Adoption/app/app.py
@@ -1,8 +1,7 @@
 import streamlit as st
-import numpy as np
-import pandas as pd
 import requests
 import os
+import pickle
 
 curr_path = os.path.dirname(__file__)
 
@@ -12,24 +11,33 @@ st.set_page_config(
     page_icon = '🐶🐱',
     layout = 'wide', #centered
     initial_sidebar_state = 'auto', # collapsed
+    )
 
-)
-
-
-CSS_dog = """
+CSS_hero = """
 .block-container {
-    background-color: rgba(255, 255, 255, 0.5);
+    background-color: rgba(253,252,245, 0.4);
     color: rgba(0, 0, 0, 1);
 }
 .stApp {
-    background-image: url(https://tractive.com/blog/wp-content/uploads/2016/04/puppy-care-guide-for-new-parents.jpg);
+    background-image: url('https://www.vmamodesto.com/images/page-heroes/hero-puppykitten.jpg');
+    background-size: cover;
+}
+"""
+
+CSS_dog = """
+.block-container {
+    background-color: rgba(253,252,245, 0.4);
+    color: rgba(0, 0, 0, 1);
+}
+.stApp {
+    background-image: url('https://wallpapercave.com/uwp/uwp1908294.jpeg');
     background-size: cover;
 }
 """
 
 CSS_cat = """
 .block-container {
-    background-color: rgba(255, 255, 255, 0.5);
+    background-color: rgba(253,252,245, 0.4);
     color: rgba(0, 0, 0, 1);
 }
 .stApp {
@@ -37,7 +45,6 @@ CSS_cat = """
     background-size: cover;
 }
 """
-
 #Title
 '''# 🐕🐩 Animals deserve a home 🐈‍⬛🐈'''
 
@@ -45,11 +52,13 @@ CSS_cat = """
 st.markdown('#### A project to help animal shelters worldwide')
 
 st.write(' ###### By providing some information, our website can assist you in determining how long a dog or cat will stay in your shelter')
-st.write('First, you need to specify if the animal is a dog or a cat.')
-choose = st.radio('',('','🐩 Dog','🐈 Cat'))
 
+
+st.write('### First, you need to specify if the animal is a dog or a cat.')
+choose = st.radio('',('🐩 Dog','🐈 Cat'))
 if choose == '':
-    pass
+    st.write(f'<style>{CSS_hero}</style>', unsafe_allow_html=True)
+    animal_type = ''
 elif choose == '🐩 Dog':
     st.write(f'<style>{CSS_dog}</style>', unsafe_allow_html=True)
     animal_type = 'Dog'
@@ -66,16 +75,16 @@ st.write("### Can you tell me more about how this animal was found?")
 col1, col2, col3 = st.columns((2,2,3))
 
 #check the column name and what I wrote here
-intake_type = col1.selectbox("Pick one of those options about how this animal ended up in your shelter?",\
-    ('Public Assist', 'Owner Surrender', 'Stray', 'Euthanasia Request','Abandoned'))
+# intake_type = col1.selectbox("Pick one of those options about how this animal ended up in your shelter?",\
+#     ('Public Assist', 'Owner Surrender', 'Stray', 'Euthanasia Request','Abandoned'))
 
 intake_condition = col2.selectbox("Now plase tell us about this animal health conditions",\
     ('Normal', 'Injured', 'Aged', 'Sick', 'Other', 'Medical', 'Feral', 'Pregnant', 'Nursing', 'Behavior'))
 
 
 
-st.write("### The age of this animal in years and months")
-col1, col2, col3 = st.columns((1.5,1.5,5))
+st.write("### How old is this animal? And does it look like it has a breed?")
+col1, col2, col3, col4 = st.columns((1.5,1.5,2, 5))
 
 years = col1.number_input('Years',min_value = 0, max_value = 28,step=1,value=0)
 
@@ -84,16 +93,13 @@ if years == 0:
 else:
     months = col2.number_input('Months',min_value = 0, max_value = 11,step=1,value=1)
 
-age_upon_intake_months = years * 12 + months
-
+age_upon_intake_years = years+ months/12
+breed = col3.selectbox("Breed",('Common', 'Uncommon'))
 
 
 st.write("### Animal Characteristics")
 col1, col2, col3, col4, col5= st.columns((2,1.5,1.5,3,5))
-
-
-
-good_with_other_dogs = col1.selectbox("Friendliness with dogs",('1','2', '3', '4', '5'))
+#good_with_other_dogs = col1.selectbox("Friendliness with dogs",('1','2', '3', '4', '5'))
 
 color = col2.selectbox("Color or pattern",('Black', 'White', 'Brown', 'Beige','Has Spots', 'Tabby or Brindle', 'Tricolor'))
 
@@ -116,69 +122,35 @@ st.write(f"### Please press the button to check if the animal will stay longer t
 # Prediction
 
 url = 'https://animal-api-gzsqtwobpa-lz.a.run.app/predict'
-
+url_local = "http://127.0.0.1:8000/predict"
 if st.button('Click here'):
     if choose == '':
-        st.warning('You forgot to choose an animal!')
+        st.warning("No, no, no. You need to pick 'Dog' or 'Cat'")
     else: #please make sure this is correct with the model!!!!!
-        params = {"intake_type": intake_type,
+        params = {
+            "age_upon_intake_y": age_upon_intake_years,
             "animal_type": animal_type,
             "intake_condition": intake_condition,
             "breed": breed,
-            "age_upon_intake_months": age_upon_intake_months,
-            "neutered_or_spayed_intake": neutered_or_spayed_intake,
-            "male_or_female_intake": male_or_female_intake,
+            "sex": male_or_female_intake,
+            "sex_type": neutered_or_spayed_intake,
             "color": color
         }
 
-        response = requests.get(url, params=params)
-        if response.json()["prediction"] == 0:
-            st.success("This animal is likely to stay less than one week in your shelter!")
-        else:
-            st.info('''
-            This animal is likely to stay longer 😿 Here are some things you can try:
-            - Take photos and videos of this animal to show their personality;
-            - Contact local newspapers, radio stations, and TV channels to see if they are willing to do a feature on the animal;
-            - artner with rescue groups;
-            - Social media can always be helpful! Make sure to use hashtags to increase reach.
-            '''
-            )
+        response = requests.get(url_local, params=params)
+        # st.write(response)
+        # st.write(response.json())
+        days = response.json()['days_in_shelter']
 
-
-
-
- #---------
- # TaxiFareModel front
-
-# This front queries the Le Wagon [taxi fare model API](https://taxifare.lewagon.ai/predict?pickup_datetime=2012-10-06%2012:10:20&pickup_longitude=40.7614327&pickup_latitude=-73.9798156&dropoff_longitude=40.6513111&dropoff_latitude=-73.8803331&passenger_count=2)
-# '''
-
-# with st.form(key='params_for_api'):
-
-#     pickup_date = st.date_input('pickup datetime', value=datetime.datetime(2012, 10, 6, 12, 10, 20))
-#     pickup_time = st.time_input('pickup datetime', value=datetime.datetime(2012, 10, 6, 12, 10, 20))
-#     pickup_datetime = f'{pickup_date} {pickup_time}'
-#     pickup_longitude = st.number_input('pickup longitude', value=40.7614327)
-#     pickup_latitude = st.number_input('pickup latitude', value=-73.9798156)
-#     dropoff_longitude = st.number_input('dropoff longitude', value=40.6413111)
-#     dropoff_latitude = st.number_input('dropoff latitude', value=-73.7803331)
-#     passenger_count = st.number_input('passenger_count', min_value=1, max_value=8, step=1, value=1)
-
-#     st.form_submit_button('Make prediction')
-
-# params = dict(
-#     pickup_datetime=pickup_datetime,
-#     pickup_longitude=pickup_longitude,
-#     pickup_latitude=pickup_latitude,
-#     dropoff_longitude=dropoff_longitude,
-#     dropoff_latitude=dropoff_latitude,
-#     passenger_count=passenger_count)
-
-# wagon_cab_api_url = 'https://taxifare.lewagon.ai/predict'
-# response = requests.get(wagon_cab_api_url, params=params)
-
-# prediction = response.json()
-
-# pred = prediction['fare']
-
-# st.header(f'Fare amount: ${round(pred, 2)}')
+        st.write(f'The animal will stay around {days} days')
+        # if response.json()['days_in_shelter'] == 0:
+        #     st.success("This animal is likely to stay less than one week in your shelter!")
+        # else:
+        #     st.info('''
+        #     This animal is likely to stay longer 😿 Here are some things you can try:
+        #     - Take photos and videos of this animal to show their personality;
+        #     - Contact local newspapers, radio stations, and TV channels to see if they are willing to do a feature on the animal;
+        #     - artner with rescue groups;
+        #     - Social media can always be helpful! Make sure to use hashtags to increase reach.
+        #     '''
+        #     )
